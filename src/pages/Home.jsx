a231714d55790ddd4ee3b8454ea35e2eb99a28c7
sakibbinkibria <52743094+sakibbinkibria@users.noreply.github.com>
2025-06-14 19:29:08 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+//import Navbar from '../components/Navbar';
 import Hero from '../sections/Hero';
-import FeaturedWork from '../sections/FeaturedWork';
-import Gallery from '../sections/Gallery';
-import MyStory from '../sections/MyStory';
+// import FeaturedWork from '../sections/FeaturedWork';
+// import Gallery from '../sections/Gallery';
+// import MyStory from '../sections/MyStory';
 import './Home.css'; // We'll add scroll snapping styles here
 
 const Home = () => {
